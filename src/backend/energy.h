44f1827516b84dd5c65fb0088512e13d16872a6f
Matheus de Paula Medeiros <matheus.de.paula.medeiros@gmail.com>
2021-04-05 13:22:14 -0300

    Add missing struct definition for energy rate

--- src/backend/energy.h
+++ src/backend/energy.h
@@ -1,7 +1,23 @@
 #ifndef ENERGY_H
 #define ENERGY_H
 
+#include <time.h>
+
 #include "power.h"
+
+typedef struct energy_rate_s {
+	time_t start_timestamp;
+	double rate;
+	int is_tou;
+	double peak_rate;
+	double mid_rate;
+	int peak_start;
+	int peak_end;
+	int mid_start;
+	int mid_end;
+	time_t creation_date;
+	time_t modification_date;
+} energy_rate_t;
 
 int energy_add_power(power_data_t *pd);
 
